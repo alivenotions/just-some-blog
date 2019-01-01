a8b314f6e82cca7847451212e9747531ce52f8a7
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2019-01-01 17:55:21 +0530

    draft of first post

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,12 @@ import React from 'react'
 class Footer extends React.Component {
   render() {
     return (
-      <footer>
-        Made with love/
+      <footer
+        style={{
+          fontSize: `0.8em`
+        }}
+      >
+        The end.
       </footer>
     )
   }
